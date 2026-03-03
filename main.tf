f1b40fd0cfd25f93f81008ce643d561f6d02e402
rayane <rayane.santana_@hotmail.com>
2026-03-03 18:43:24 -0300

    Configura backend remoto S3

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-terraform-state-rayane"
+    key            = "global/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
