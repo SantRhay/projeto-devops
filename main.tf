cf37b1de50701e920859d40b9bde1eeaa7bf2d60
rayane <rayane.santana_@hotmail.com>
2026-03-03 18:21:20 -0300

    Troca para t3.micro

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 
 resource "aws_instance" "devops_ec2" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "DevOps-Projeto-Rayane"
